a1fa86c84465480262bde83891267ba14f962df7
Norris Lau <liunian@2077tech.com>
2023-09-13 17:00:39 +0800

    feat: 新增自动生成 package.json 类型的脚本

---
+++ types/__quicktype__/package.d.ts
@@ -0,0 +1,111 @@
+export interface PackageD {
+    name:               string;
+    version:            string;
+    private:            boolean;
+    license:            string;
+    packageManager:     string;
+    repository:         Repository;
+    engines:            Engines;
+    scripts:            Scripts;
+    dependencies:       Dependencies;
+    devDependencies:    DevDependencies;
+    "simple-git-hooks": SimpleGitHooks;
+    config:             Config;
+}
+
+export interface Config {
+    commitizen: Commitizen;
+}
+
+export interface Commitizen {
+    path: string;
+}
+
+export interface Dependencies {
+    echarts:        string;
+    "element-plus": string;
+    "lodash-es":    string;
+    pinia:          string;
+    tslib:          string;
+    "vue-echarts":  string;
+}
+
+export interface DevDependencies {
+    "@commitlint/cli":                  string;
+    "@commitlint/config-conventional":  string;
+    "@element-plus/nuxt":               string;
+    "@mockoon/cli":                     string;
+    "@nuxt/devtools":                   string;
+    "@nuxtjs/color-mode":               string;
+    "@nuxtjs/eslint-config-typescript": string;
+    "@nuxtjs/i18n":                     string;
+    "@pinia/nuxt":                      string;
+    "@semantic-release/changelog":      string;
+    "@semantic-release/exec":           string;
+    "@semantic-release/git":            string;
+    "@semantic-release/gitlab":         string;
+    "@semantic-release/gitlab-config":  string;
+    "@types/lodash-es":                 string;
+    "@types/node":                      string;
+    "@vueuse/components":               string;
+    "@vueuse/core":                     string;
+    "@vueuse/nuxt":                     string;
+    autoprefixer:                       string;
+    commander:                          string;
+    concurrently:                       string;
+    daisyui:                            string;
+    eslint:                             string;
+    "eslint-config-prettier":           string;
+    "eslint-plugin-import":             string;
+    "eslint-plugin-jsdoc":              string;
+    "eslint-plugin-jsonc":              string;
+    "git-cz":                           string;
+    "icon-gen":                         string;
+    "lint-staged":                      string;
+    nuxt:                               string;
+    "nuxt-icon":                        string;
+    "nuxt-vite-legacy":                 string;
+    postcss:                            string;
+    prettier:                           string;
+    quicktype:                          string;
+    sass:                               string;
+    sharp:                              string;
+    "simple-git-hooks":                 string;
+    "swagger-typescript-api":           string;
+    tailwindcss:                        string;
+    taze:                               string;
+    terser:                             string;
+    "unplugin-auto-import":             string;
+    "unplugin-vue-components":          string;
+    "vue-tsc":                          string;
+}
+
+export interface Engines {
+    node: string;
+}
+
+export interface Repository {
+    type: string;
+    url:  string;
+}
+
+export interface Scripts {
+    preinstall:          string;
+    postinstall:         string;
+    "nuxt-prepare":      string;
+    build:               string;
+    dev:                 string;
+    "nuxt-dev":          string;
+    "upgrade-dep":       string;
+    cz:                  string;
+    "semantic-release":  string;
+    "icon-gen":          string;
+    lint:                string;
+    "lint-fix":          string;
+    "generate-api-type": string;
+    "generate-pkg-type": string;
+}
+
+export interface SimpleGitHooks {
+    "pre-commit": string;
+}
