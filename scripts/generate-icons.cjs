f5f4f560245e77fb29a8f6c6668217da4e0992d3
Norris Lau <liunian@2077tech.com>
2023-09-14 14:33:49 +0800

    chore: 图标生成脚本改为传参模式

--- scripts/generate-icons.cjs
+++ scripts/generate-icons.cjs
@@ -1,20 +1,28 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const icongen = require('icon-gen');
+const { program } = require('commander');
 
+program
+  .requiredOption('-i, --input  <string>', '输入的文件路径,支持 svg/png 格式。')
+  .requiredOption('-o, --output  <string>', '导出的路径。')
+  .requiredOption('-p, --prefix  <string>', '图片的前缀。');
+
+program.parse();
+const options = program.opts();
 const iconConfig = require('../icon.config.json');
 
-const inputFile = path.join(__dirname, '../public/favicon.svg');
-const outputFolder = path.join(__dirname, '../public/images');
+const inputFile = path.resolve(process.cwd(), options.input);
+const outputFolder = path.resolve(process.cwd(), options.output);
 
 /**
- *
+ * 生成图标
  */
 async function generateIcons() {
   await icongen(inputFile, outputFolder, {
     report: true,
     favicon: {
-      name: 'favicon-',
+      name: options.prefix,
       pngSizes: [
         ...new Set([...iconConfig.android.sizes, ...iconConfig.ios.sizes])
       ].sort((a, b) => a - b),
