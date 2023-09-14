5809a67d9c8371d9770bbafcbe48287dc88dc58a
Norris Lau <liunian@2077tech.com>
2023-09-14 14:32:43 +0800

    chore: 添加注释

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,6 +1,17 @@
 module.exports = {
+  /**
+   * 使用单引号
+   */
   singleQuote: true,
-  printWidth: 80,
+  /**
+   * 箭头函数在可能的情况下省略括号
+   * @see https://prettier.io/docs/en/options.html#arrow-function-parentheses
+   * @example x => x
+   */
   arrowParens: 'avoid',
+  /**
+   * 在多行逗号分隔的语法结构中，不打印尾随逗号。
+   * @see https://prettier.io/docs/en/options.html#trailing-commas
+   */
   trailingComma: 'none'
 };
