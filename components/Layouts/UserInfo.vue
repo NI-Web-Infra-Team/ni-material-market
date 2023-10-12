89c64adac91fb260e11b5160771f39577f20d5c9
liunian <liunian7965@navinfo.com>
2023-10-12 17:20:30 +0800

    chore: 移除无用代码

--- components/Layouts/UserInfo.vue
+++ components/Layouts/UserInfo.vue
@@ -1,19 +1,5 @@
 <script lang="ts" setup>
 import { UseImage } from '@vueuse/components';
-
-const { t } = useI18n();
-const router = useRouter();
-const dialog = ref(false);
-
-/**
- * 退出登录
- */
-async function handleSignOut() {
-  try {
-    dialog.value = false;
-    await router.replace({ path: getRouterByName('index').path });
-  } catch {}
-}
 </script>
 <template>
   <v-menu>
@@ -39,20 +25,6 @@ async function handleSignOut() {
           <p>admin</p>
         </div>
       </v-list-item>
-      <v-divider></v-divider>
-      <v-list-item @click="dialog = true">
-        <v-list-item-title>{{ t('signIn.signOut') }}</v-list-item-title>
-      </v-list-item>
     </v-list>
   </v-menu>
-  <v-dialog v-model="dialog" persistent class="tw-w-96">
-    <v-card>
-      <v-card-title class="text-h5">{{ t('signIn.signOut') }}</v-card-title>
-      <v-card-text>{{ t('signIn.signOutConfirm') }}</v-card-text>
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn variant="text" @click="dialog = false">{{ t('cancel') }}</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
 </template>
