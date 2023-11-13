c19c25b4ead0c57b35f319e8f71954ecee04b7ae
Norris Lau <liunian@2077tech.com>
2023-11-13 17:49:43 +0800

    refactor: format

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { fileURLToPath, URL } from 'node:url'
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'node:url';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls },
+      template: { transformAssetUrls }
     }),
     vueJsx(),
-    vuetify({ autoImport: true }),
+    vuetify({ autoImport: true })
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+});
